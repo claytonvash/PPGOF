18696b02dfe2785ef6742e77e3421139fe553893
clayton Silva <clayton.iftm@gmail.com>
2019-05-03 17:10:56 -0300

    Criando a classe GeradorPropriedadesDefault

---
+++ src/templateMethod/GerardorPropriedadesDefault.java
@@ -0,0 +1,17 @@
+package templateMethod;
+
+import java.util.Map;
+
+public class GerardorPropriedadesDefault extends GeradorArquivo {
+
+	@Override
+	protected String gerarConteudo(Map<String,Object> propriedades) {
+		//gera properties
+			 StringBuilder propFileBuilder = new StringBuilder();
+			   for (String prop: propriedades.keySet()) {
+			    propFileBuilder.append(prop+"m"+propriedades.get(prop)+"\n");
+			   }
+			  return propFileBuilder.toString();
+	}
+
+}
