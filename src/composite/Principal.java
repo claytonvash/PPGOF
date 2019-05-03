3e1b55ed2f0e8109b82c510f4d2aa8abeb19a9e6
clayton Silva <clayton.iftm@gmail.com>
2019-05-03 20:13:07 -0300

    Finalizando a implementação do composite e testando a geração dde arquivos

--- src/composite/Principal.java
+++ src/composite/Principal.java
@@ -1,6 +1,8 @@
 package composite;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Principal {
@@ -9,15 +11,27 @@ public class Principal {
   Map<String, Object> mapa = new HashMap<>();
   mapa.put("nome", "Clayton");
   mapa.put("idade", 39);
+  List<Processador> processadores = new ArrayList<Processador>();
+     processadores.add(new ProcessaCriptografado());
+     processadores.add(new ProcessaCompactado()); 
+     
+  List<Processador> processadores1 = new ArrayList<Processador>();
+     processadores1.add(new ProcessaCompactado());
+     processadores1.add(new ProcessaCriptografado()); 
+     
+  List<Processador> processadores2 = new ArrayList<Processador>();
+     processadores2.add(new ProcessaDefault());
+     processadores2.add(new ProcessaCompactado());
   
-  GeradorArquivo gerador = new GeradorArquivoPropriedades(new ProcessaCriptografado());
-  gerador.gerarArquivo("c:\\Gerador\\Cripto.txt", mapa);
   
-  GeradorArquivo gerador1 = new GeradorArquivoXml(new ProcessaCompactado());
-  gerador1.gerarArquivo("c:\\Gerador\\XML.zip", mapa);  
+  GeradorArquivo gerador = new GeradorArquivoPropriedades(new ProcessadorComposto(processadores));
+  gerador.gerarArquivo("c:\\Gerador\\Prop1.zip", mapa);
   
-  GeradorArquivo gerador2 = new GeradorArquivoPropriedades(new ProcessaDefault());
-  gerador2.gerarArquivo("c:\\Gerador\\Default.txt", mapa);
+  GeradorArquivo gerador1 = new GeradorArquivoXml(new ProcessadorComposto(processadores1));
+  gerador1.gerarArquivo("c:\\Gerador\\Xml1.txt", mapa);  
+  
+  GeradorArquivo gerador2 = new GeradorArquivoPropriedades(new ProcessadorComposto(processadores2));
+  gerador2.gerarArquivo("c:\\Gerador\\Default.zip", mapa);
  }
 
 }
