10c6fe882c24a7ccebfe675d7dc9249cb8d14895
clayton Silva <clayton.iftm@gmail.com>
2019-05-03 14:41:59 -0300

    Criando a classe ProcessaXmlCompactado e ajustando a classe GeradorArquivo

--- src/estruturado/GeradorArquivo.java
+++ src/estruturado/GeradorArquivo.java
@@ -1,22 +1,21 @@
 package estruturado;
 
-import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Map;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 public class GeradorArquivo {
  
  public final void gerarArquivo(String nome, Map<String,Object> propriedades, String tipo) throws IOException {
   byte[] bytes = null;
   if (tipo.equals("PROPRIEDADES_CRIPTOGRAFADO")) {
+	  ProcessaPropriedadesCriptografado propCript = new ProcessaPropriedadesCriptografado();
    
-   bytes = processaPropriedadesCriptografado(propriedades);
+   bytes = propCript.processaPropriedadesCriptografado(propriedades);
   } else if (tipo.equals("XML_COMPACTADO")) {
+	  ProcessaXmlCompactado xml = new ProcessaXmlCompactado();
    
-   bytes = processaXmlCompactado(propriedades);
+   bytes = xml.processaXmlCompactado(propriedades);
   } else {
    System.out.println("Desconheço essa opção");
   }
@@ -24,33 +23,5 @@ public class GeradorArquivo {
   fileout.write(bytes);
   fileout.close();
  }
- private byte[] processaXmlCompactado(Map<String,Object> propriedades)throws IOException{
-	 String conteudo = geraConteudoXml(propriedades);
-	 return processaCompactacao(conteudo.getBytes());
-	 
-}
- 
-private String geraConteudoXml(Map<String,Object> propriedades) {
-	//gera xml
-	   StringBuilder propFileBuilder = new StringBuilder();
-	   propFileBuilder.append("<properties>");
-	   for (String prop: propriedades.keySet()) {
-	    propFileBuilder.append("<"+prop+">"+propriedades.get(prop)+"</"+prop+">");
-	   }
-	   propFileBuilder.append("</propriedades>");
-	   return propFileBuilder.toString();
-}
-
-private byte[] processaCompactacao(byte[] bytes) throws IOException {
-	//compacta
-	   ByteArrayOutputStream byteout = new ByteArrayOutputStream();
-	   ZipOutputStream out = new ZipOutputStream(byteout);
-	   out.putNextEntry(new ZipEntry("internal"));
-	   out.write(bytes);
-	   out.closeEntry();
-	   out.close();
-	   return  byteout.toByteArray();
-	
-}
  
 }
