1f6314c4b817aaf5030da3e3a2f5fd051a3b407a
clayton Silva <clayton.iftm@gmail.com>
2019-05-03 21:47:44 -0300

    Implementando a notificação por lista

--- src/observer/GeradorArquivo.java
+++ src/observer/GeradorArquivo.java
@@ -2,12 +2,17 @@ package observer;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public abstract class GeradorArquivo {
 	private Processador processador;
+	private List<Observador> observadores;
 	public GeradorArquivo(Processador processador) {
 		this.processador = processador;
+		this.observadores = new ArrayList<Observador>();
+		
 	}
  
  public final void gerarArquivo(String nome, Map<String,Object> propriedades) throws IOException {
@@ -28,6 +33,13 @@ public abstract class GeradorArquivo {
   FileOutputStream fileout = new FileOutputStream(nome);
   fileout.write(bytes);
   fileout.close();
+  notificar(nome,conteudo);
+ }
+ public void notificar(String nomeArquivo, String conteudo) {
+	 for(Observador observador: observadores) {
+		 observador.novoArquivoGerado(nomeArquivo,conteudo);
+	 }
+	 
  }
  protected abstract String gerarConteudo(Map<String,Object> propriedades);
  
